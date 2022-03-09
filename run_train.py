a64335cf3fbf685022b4c67e92f3d4241192eda0
zeynep068 <zeynep.boztoprak@hhu.de>
2022-03-09 22:44:57 +0100

    debug

--- run_train.py
+++ run_train.py
@@ -1,16 +1,20 @@
+import torch
 from torch.utils.data import DataLoader
 from augmentation import DataAugmentation
 from torchvision.datasets import STL10
 
 
+def set_device():
+    return torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
 def load_unlabeled_data(batch_size, num_workers):
-    transform = DataAugmentation()
-    trainset = STL10(root='./data', download=True, transform=transform,
+    trainset = STL10(root='./data', download=True, transform=DataAugmentation(),
                      split='unlabeled')
-    return dataloader(trainset, batch_size, num_workers)
+    return get_dataloader(trainset, batch_size, num_workers)
 
 
-def dataloader(dataset, batch_size, num_workers):
+def get_dataloader(dataset, batch_size, num_workers):
     return DataLoader(dataset, batch_size=batch_size, shuffle=True,
                       num_workers=num_workers)
 
@@ -18,14 +22,17 @@ def dataloader(dataset, batch_size, num_workers):
 def train(config):
     train = load_unlabeled_data(256, config['num_workers'])
 
-    for idx, data in enumerate(train):
+    for data in train:
         inputs, labels = data
-        print(f"{idx}: {inputs.shape}")
+        print(labels)
+        exit()
+
+        inputs = inputs.to(config['device'])
 
 
 def config():
     return {'batch_size': 64, 'num_workers': 1, 'T': 1., 'epochs': 100,
-            'lr': 4.8, 'weight_decay': 10e-6}
+            'lr': 4.8, 'weight_decay': 10e-6, 'device': set_device()}
 
 
 if __name__ == "__main__":
