f263f271739fedc6ca181c7ea325deed9d58bbef
zeynep068 <zeynep.boztoprak@hhu.de>
2022-03-09 22:48:23 +0100

    debug

--- augmentation.py
+++ augmentation.py
@@ -14,5 +14,5 @@ class DataAugmentation:
                                     T.Normalize(mean=(0.485, 0.456, 0.406),
                                                 std=(0.229, 0.224, 0.225))])
 
-    def foo(self, x):
+    def __call__(self, x):
         return self.transform(x)
