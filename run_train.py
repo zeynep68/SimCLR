490b362a3889451cfe6d363d39ee25c77f940db2
zeynep068 <zeynep.boztoprak@hhu.de>
2022-03-09 23:27:08 +0100

    add: training step and loss call

--- run_train.py
+++ run_train.py
@@ -1,9 +1,13 @@
 import torch
 import random
 import numpy as np
+from torch.optim import Adam
 from torch.utils.data import DataLoader
 from augmentation import DataAugmentation
 from torchvision.datasets import STL10
+
+from simclr import SimCLRNet
+from loss import ContrastiveLoss
 
 
 def set_seed(seed=225):
@@ -33,12 +37,29 @@ def get_dataloader(dataset, batch_size, num_workers):
 def train_one_epoch(config):
     train = load_unlabeled_data(256, config['num_workers'])
 
+    model = SimCLRNet()
+    criterion = ContrastiveLoss()
+    optimizer = Adam(model.parameters())
+
     for (view1, view2), _ in train:
         view1 = view1.to(config['device'])
         view2 = view2.to(config['device'])
 
+        train_step(model, optimizer, criterion, view1, view2)
 
-def config():
+
+def train_step(model, optimizer, criterion, view1, view2):
+    optimizer.zero_grad()
+
+    outputs1 = model(view1)
+    outputs2 = model(view2)
+
+    loss = criterion(outputs1, outputs2)
+    loss.backward()
+    optimizer.step()
+
+
+def get_config():
     return {'batch_size': 64, 'num_workers': 1, 'T': 1., 'epochs': 100,
             'lr': 4.8, 'weight_decay': 10e-6, 'device': set_device()}
 
@@ -50,4 +71,4 @@ def train(config):
 
 if __name__ == "__main__":
     set_seed()
-    train(config())
+    train(get_config())
