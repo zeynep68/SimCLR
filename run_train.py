d0dcd967c7cae67b07108ddc601c309cd16fafab
zeynep068 <zeynep.boztoprak@hhu.de>
2022-03-09 22:55:46 +0100

    add: seed

--- run_train.py
+++ run_train.py
@@ -1,34 +1,9 @@
 import torch
+import random
+import numpy as np
 from torch.utils.data import DataLoader
 from augmentation import DataAugmentation
 from torchvision.datasets import STL10
-
-"""
-import random
-import numpy as np
-import torch.optim as optim
-from simclr import SimCLRNet
-
-def train2(config):
-    train = load_unlabeled_data(100000, config['num_workers'])
-
-    model = SimCLRNet()
-
-    optimizer = optim.Adam(model.parameters(), lr=config['lr'])
-
-    for epoch in range(config['epochs']):
-        for data in train:  # batch-wise
-            inputs, labels = data
-            print('fo ')
-            print('data shape:', data.shape)
-            print('mean:', np.mean(inputs, axis=1))
-            print('std:', np.std(inputs, axis=1))
-
-            optimizer.zero_grad()  # print(inputs.shape)  # preds = model(  #  
-            # inputs)  # exit()
-        exit()
-
-    return
 
 
 def set_seed(seed=225):
@@ -38,8 +13,6 @@ def set_seed(seed=225):
 
     random.seed(seed)
     np.random.seed(seed)
-
-"""
 
 
 def set_device():
@@ -60,12 +33,9 @@ def get_dataloader(dataset, batch_size, num_workers):
 def train(config):
     train = load_unlabeled_data(256, config['num_workers'])
 
-    for (view1, view2), t in train:
+    for (view1, view2), _ in train:
         view1 = view1.to(config['device'])
         view2 = view2.to(config['device'])
-        print(view1.shape)
-        print(view2.shape)
-        print('t:', t)
 
 
 def config():
