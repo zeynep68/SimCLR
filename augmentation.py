e7a62871bc37691106d31446a6bf319c44d02019
zeynep068 <zeynep.boztoprak@hhu.de>
2022-03-09 22:00:48 +0100

    add: data augmentation module

---
+++ augmentation.py
@@ -0,0 +1,18 @@
+import torchvision.transforms as T
+
+
+class DataAugmentation:
+    """ A stochastic data augmentation module. """
+
+    def __init__(self, img_size=96):
+        gaussian_blur = T.GaussianBlur(kernel_size=3)
+
+        self.transform = T.Compose([T.ToTensor(), T.RandomResizedCrop(img_size),
+                                    T.RandomHorizontalFlip(0.5),
+                                    T.RandomGrayscale(0.5),
+                                    T.RandomApply([gaussian_blur], 0.5),
+                                    T.Normalize(mean=(0.485, 0.456, 0.406),
+                                                std=(0.229, 0.224, 0.225))])
+
+    def __call__(self, x):
+        return self.transform(x)
