ee58da5577e81dce0543e1f97d3a5c27ec79c3dc
zeynep068 <zeynep.boztoprak@hhu.de>
2022-03-29 23:06:06 +0200

    fix: use view instead of flatten

--- simclr.py
+++ simclr.py
@@ -7,6 +7,7 @@ class Encoder(nn.Module):
         super().__init__()
 
         self.model = resnet18()
+        # TODO: extract method
         self.model = nn.Sequential(*list(self.model.children())[:-1])
         # self.model.fc = nn.Identity()
 
@@ -17,18 +18,19 @@ class Encoder(nn.Module):
 class ProjectionHead(nn.Module):
     def __init__(self, resnet_dim, embedding_dim):
         super().__init__()
-        self.head = nn.Sequential(nn.Flatten(),
-                                  nn.Linear(resnet_dim, resnet_dim),
+        self.head = nn.Sequential(nn.Linear(resnet_dim, resnet_dim),
                                   nn.ReLU(),
                                   nn.Linear(resnet_dim, embedding_dim))
 
     def forward(self, x):
+        x = x.view(x.shape[0], -1)
         return self.head(x)
 
 
 class SimCLRNet(nn.Module):
     def __init__(self, embedding_dim=128, resnet_dim=512, pretrain=True):
         super().__init__()
+        # TODO: remove parameter; instead make another function predict?
         self.pretrain = pretrain
 
         self.encoder = Encoder()
