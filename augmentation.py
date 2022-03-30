b03ee6958dd13cd64f991e5e30437f9fc030c06e
zeynep068 <zeynep.boztoprak@hhu.de>
2022-03-30 16:52:00 +0200

    update: greyscale is less probable

--- augmentation.py
+++ augmentation.py
@@ -9,8 +9,8 @@ class DataAugmentation:
 
         self.transform = T.Compose([T.ToTensor(), T.RandomResizedCrop(img_size),
                                     T.RandomHorizontalFlip(0.5),
-                                    T.RandomGrayscale(0.5),
                                     T.RandomApply([gaussian_blur], 0.5),
+                                    T.RandomGrayscale(0.2),
                                     T.Normalize(mean=(0.485, 0.456, 0.406),
                                                 std=(0.229, 0.224, 0.225))])
 
