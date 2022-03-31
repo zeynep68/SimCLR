1457d3b9190fd8ee8a5f481c4054212adfaa9adc
zeynep068 <zeynep.boztoprak@hhu.de>
2022-03-31 13:05:40 +0200

    test: save function

--- run_train.py
+++ run_train.py
@@ -22,7 +22,7 @@ def get_config():
     return {'batch_size': 256, 'num_workers': 2, 'temperature': 0.5,
             'epochs': 50, 'lr': 3e-4, 'weight_decay': 1e-6,
             'device': set_device(), 'num_views': 2, 'pin_memory': True,
-            'use_wandb': True, 'save_model': False, 'load_model': False}
+            'use_wandb': True, 'save_model': True, 'load_model': False}
 
 
 def main():
